1011ccd63b849ddc073c519e9655d677579e21f3
Rakesh <rakeshparihargarur@gmail.com>
2019-01-22 09:22:46 +0530

    Initial commit

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+    public GameObject _tile;
+    public float _offsetValue;
+    Vector3 tilePos = Vector3.zero;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GridGenerate();
+    }
+	
+	private void GridGenerate()
+    {
+        for(int i=0; i< 3; i++)
+        {
+           
+            for(int j=0; j< 3; j++)
+            {
+                tilePos.x = transform.position.x + j;
+                tilePos.y = transform.position.y - i;
+                GameObject tileInstance = Instantiate(_tile, tilePos * _offsetValue, Quaternion.identity);
+                tileInstance.transform.SetParent(this.transform);
+            }
+        }
+    }
+}
